92364830f730fee80743a403eadba0278476cdd8
Stefan Bauwens <stefan.bauwens@hotmail.com>
2017-03-02 20:55:58 +0100

    Updated class diagram

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Settings : Popup {
-    protected bool musicOn;
-    protected bool soundOn;
+public class Pause : Popup {
 
 	// Use this for initialization
 	void Start () {
