5561dab8807adc972183537614ffc2871d403dd2
cindyho97 <cindy.ho8197@gmail.com>
2017-03-14 09:15:22 +0100

    change speedchange class name

--- Assets/Scripts/Speedchange.cs
+++ Assets/Scripts/Speedchange.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpeedChange : Powerup {
-    public float speedModifier;
+public class SpeedChange : Powerup{
 
 	// Use this for initialization
 	void Start () {
