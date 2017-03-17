cec95d1b441dc69bb35c9504cf113f454de22a7b
cindyho97 <cindy.ho8197@gmail.com>
2017-03-17 17:22:32 +0100

    add game over

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class GameOver : Popup {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public bool isDead = false;
+    public GameObject gameOver;
+
+    void GameOverPopup()
+    {
+        if (isDead)
+        {
+            gameOver.SetActive(true);
+        }
+    }
+
 }
