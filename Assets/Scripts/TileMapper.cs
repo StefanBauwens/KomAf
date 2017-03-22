2318d7feff13c757cf8d5135fd9794d34077fa05
Stefan Bauwens <stefan.bauwens@hotmail.com>
2017-03-22 18:53:26 +0100

    Modified tilemapper code and cleaned it up

--- Assets/Scripts/TileMapper.cs
+++ Assets/Scripts/TileMapper.cs
@@ -3,48 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TileMapper : MonoBehaviour {
-	//public GameObject RedObject; //please use objects with all the same width & height for no problems
-	//public GameObject GreenObject;
-
 	public GameObject[] gameObjects;
-	public Vector3[] Colours;//X = r Y = g Z = b
 	public string[] hexColours;
-	//public GameObject defaultObject;
 	public Texture2D Map;
 
 	protected Color[] realColours;
-
 	protected GameObject tempObject;
-	//protected Color RED = new Color (1f, 0, 0);
-	//protected Color GREEN = new Color (0, 1f, 0);
 	protected Color[] colorArray;
 
 	// Use this for initialization
 	void Start () {
-		//converts the vector3's to colours:
-		realColours = new Color[Colours.Length];
+		realColours = new Color[hexColours.Length];
 		for (int i = 0; i < hexColours.Length; i++) {
-			//realColours [i] = new Color (Colours [i].x/255f, Colours [i].y/255f, Colours [i].z/255f);
-			ColorUtility.TryParseHtmlString ("#"+hexColours[i], out realColours[i]);
+			ColorUtility.TryParseHtmlString ("#"+hexColours[i], out realColours[i]); //convert hex to colour
 		}
 
 		colorArray = Map.GetPixels(); //faster than getpixel
 		for (int height = 0; height < Map.height; height++) {
 			for (int width = 0; width < Map.width; width++) {
 				tempObject = null;
-				for (int i = 0; i < realColours.Length; i++) {
+
+				for (int i = 0; i < realColours.Length; i++) { //check every colour
 					if (colorArray [width + (height * Map.width)] == realColours[i]) {
 						tempObject = gameObjects[i];
+						break;
 					}
 				}
-				/*if (colorArray[width+ (height*Map.width)] == RED) {
-					tempObject = RedObject;
-				} else if (colorArray[width+ (height*Map.width)] == GREEN) {
-					tempObject = GreenObject;
-				} else {
-					//tempObject = defaultObject;
-					tempObject = null;
-				}*/
 
 				if (tempObject != null) {
 					Instantiate (tempObject, new Vector2 (width, height), Quaternion.identity);
