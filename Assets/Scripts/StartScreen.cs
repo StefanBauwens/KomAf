36f5a0e76a794917b543b40116f84a07d757f578
Stefan Bauwens <stefan.bauwens@hotmail.com>
2017-03-25 11:53:12 +0100

    Loadlevel works in startscreen

--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartScreen : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		Debug.Log ("start");
-	}
+	public string LevelToLoad;
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.touches.Length>0) {
-			Debug.Log ("You tapped");
+			SceneManager.LoadScene (LevelToLoad);
+			
+			//Debug.Log ("You tapped");
 		}
 	}
 }
