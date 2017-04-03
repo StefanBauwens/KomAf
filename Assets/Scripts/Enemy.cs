d4b11ef2e97d0b370f5e3f8d0e4b3b4d82fa0e4f
Stefan Bauwens <stefan.bauwens@hotmail.com>
2017-04-03 14:48:49 +0200

    Added enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	//protected Animator anim;
+	public bool isWalkingRight; //change back to protected after debugging!
+	protected Rigidbody2D rb;
+
+	public float moveSpeed = 5;
+	protected int collisionCount = 0;
+
+	public bool isGrounded;
+
+	// Use this for initialization
+	void Start () {
+		isWalkingRight = true;
+		//anim = gameObject.GetComponent<Animator> ();
+		rb 	 = gameObject.GetComponent<Rigidbody2D> ();
+		isGrounded = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.velocity = new Vector2(moveSpeed*(isWalkingRight?1:-1), rb.velocity.y);
+		if (!isGrounded) {
+			isGrounded = true;
+			isWalkingRight = !isWalkingRight;
+			transform.Rotate(new Vector3(0,180,0)); //flip the enemy
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.name == "Player") {
+			Debug.Log ("You died"); //DO GAME OVER THING HERE
+		} else {
+			Reverse ();
+		}
+	}
+
+	void Reverse()
+	{
+		isWalkingRight = !isWalkingRight;
+		transform.Rotate(new Vector3(0,180,0)); //flip the enemy
+	}
+}
---
+++ Assets/Scripts/GroundCheckEnemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheckEnemy : MonoBehaviour {
+
+	protected GameObject parent;
+	protected bool isGrounded;
+	protected int count;
+
+	void Start () {
+		parent = this.transform.parent.gameObject;
+		isGrounded = true;
+		count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isGrounded) {
+			count++;
+		}
+		//I use a counter because else every time I exit a tile and go on a tile with the same name it still thinks for a second that i need to reverse
+		if (count>1) { 
+			parent.GetComponent<Enemy> ().isGrounded = isGrounded;
+			isGrounded = true;
+			count = 0;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if(other.name == "ground8x8")
+		{
+			isGrounded = false;
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if(other.name == "ground8x8")
+		{
+			isGrounded = true;
+			count = 0;
+		}
+	}
+}
