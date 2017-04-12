183a398b6d6f5a39771c1985993ce18138f297c4
Stefan Bauwens <stefan.bauwens@hotmail.com>
2017-04-12 21:07:35 +0200

    Added popup for first time Playing

---
+++ Assets/Scripts/FirstTimePopup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirstTimePopup : MonoBehaviour {
+
+	public Button closeBtn;
+
+	// Use this for initialization
+	void Start () {
+		closeBtn.onClick.AddListener(ClosePanel);
+		if (PlayerPrefs.GetInt ("seenFirstPopup", 0) == 1) {
+			ClosePanel ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void ClosePanel()
+	{
+		PlayerPrefs.SetInt ("seenFirstPopup", 1);
+		PlayerPrefs.Save ();
+		this.gameObject.SetActive (false);
+	}
+}
