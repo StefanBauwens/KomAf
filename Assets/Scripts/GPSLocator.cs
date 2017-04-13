a714d309e6dce77621ac7c9448077fcfb1d0d352
Stefan Bauwens <stefan.bauwens@hotmail.com>
2017-04-13 14:31:29 +0200

    Added GPS script

---
+++ Assets/Scripts/GPSLocator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPSLocator : MonoBehaviour {
+
+	public static GPSLocator Instance{ get; set; }
+
+	public float longitude = 0;
+	public float latitude = 0;
+
+	public bool isBusy = true;
+
+	// Use this for initialization
+	public void Start () {
+		Instance = this;
+		DontDestroyOnLoad (gameObject);
+	}
+
+	public void RunIt()
+	{
+		StartCoroutine (StartLocationService ());
+	}
+
+
+	IEnumerator StartLocationService ()
+	{
+		if (!Input.location.isEnabledByUser) {
+			//USER HAS NOT enabled location services. Show popup asking to use it?
+			isBusy = false;
+			yield break;
+		}
+
+		Input.location.Start (3,3);
+		int maxWait = 20;
+		while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0) {
+			yield return new WaitForSeconds (1);
+			maxWait--;
+			isBusy = true;
+		}
+
+		if (maxWait <= 0 || Input.location.status == LocationServiceStatus.Failed ) {
+			//TIMED OUT SHOW POPUP Saying location was timed out or something went wrong
+			Debug.Log("Something went wrong!");
+			isBusy = false;
+			yield break;
+		}
+		longitude = Input.location.lastData.longitude;
+		latitude = Input.location.lastData.latitude;
+		Input.location.Stop ();
+		isBusy = false;
+		yield break;
+	}
+
+
+}
---
+++ Assets/Scripts/GPSTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GPSTest : MonoBehaviour {
+
+	public Text gpsText;
+	
+	// Update is called once per frame
+	void Update () {
+		if (GPSLocator.Instance.isBusy == false) {
+			GPSLocator.Instance.RunIt();
+		}
+		gpsText.text = "Lon:" + GPSLocator.Instance.longitude.ToString () + " Lat:" + GPSLocator.Instance.latitude.ToString ();
+	}
+}
