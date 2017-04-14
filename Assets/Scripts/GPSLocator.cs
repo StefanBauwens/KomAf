159a1d57dc1efabf38efb304f9bffdeac51732d9
Stefan Bauwens <stefan.bauwens@hotmail.com>
2017-04-14 13:18:42 +0200

    changed gpslocator script a bit

--- Assets/Scripts/GPSLocator.cs
+++ Assets/Scripts/GPSLocator.cs
@@ -3,27 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 //using SingleShadePlugin; //Using this plugin because of unity bug with location
 //This means that everywhere where normally "Input.location" is used it's replaced by "InputLocation"
+using UnityEngine.UI;
 
 public class GPSLocator : MonoBehaviour {
 
-	public static GPSLocator Instance{ get; set; }
+	//public GPSLocator Instance{ get; set; }
 
-	public float longitude = 0;
-	public float latitude = 0;
+	public float longitude;
+	public float latitude;
 
-	public bool isBusy = true;
+	public bool isBusy;// = true;
+
+	public Text gpsText;
+	public Text debugger;
 
 	// Use this for initialization
 	public void Start () {
-		Instance = this;
-		DontDestroyOnLoad (gameObject);
+		//Instance = this;
+		//DontDestroyOnLoad (this.transform.parent.gameObject);
 		StartCoroutine (StartLocationService ());
+		isBusy = true;
 	}
 
-	IEnumerator StartLocationService ()
+	void Update () {
+		if (!isBusy) {
+			//Start ();
+			StartCoroutine (StartLocationService ());
+		}
+		gpsText.text = "Lon:" + longitude.ToString () + " Lat:" + latitude.ToString ();
+	}
+
+	public IEnumerator StartLocationService ()
 	{
 		if (!Input.location.isEnabledByUser) {
 			//USER HAS NOT enabled location services. Show popup asking to use it?
+			debugger.text = "not enabled";
 			isBusy = false;
 			yield break;
 		}
@@ -34,19 +48,22 @@ public class GPSLocator : MonoBehaviour {
 		while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0) {
 			yield return new WaitForSeconds (1);
 			maxWait--;
+			debugger.text = "is busy";
 			isBusy = true;
 		}
 
 		if (maxWait <= 0 || Input.location.status == LocationServiceStatus.Failed ) {
 			//TIMED OUT SHOW POPUP Saying location was timed out or something went wrong
-			Debug.Log("Something went wrong!");
 			isBusy = false;
+			debugger.text = "failed";
+
 			yield break;
 		}
 		longitude = Input.location.lastData.longitude;
 		latitude = Input.location.lastData.latitude;
-		isBusy = false;
+		debugger.text = "lonlat set";
 
+		isBusy = false;
 		yield break;
 	}
 
--- Assets/Scripts/GPSTest.cs
+++ Assets/Scripts/GPSTest.cs
@@ -6,12 +6,20 @@ using UnityEngine.UI;
 public class GPSTest : MonoBehaviour {
 
 	public Text gpsText;
+	public GPSLocator gps;
 	
 	// Update is called once per frame
+
+	void Start()
+	{
+		gps.isBusy = false;
+		gps.StartCoroutine (gps.StartLocationService ());
+	}
+
 	void Update () {
-		if (GPSLocator.Instance.isBusy == false) {
-			GPSLocator.Instance.Start ();
+		if (gps.isBusy == false) {
+			gps.Start ();
 		}
-		gpsText.text = "Lon:" + GPSLocator.Instance.longitude.ToString () + " Lat:" + GPSLocator.Instance.latitude.ToString ();
+		gpsText.text = "Lon:" + gps.longitude.ToString () + " Lat:" + gps.latitude.ToString ();
 	}
 }
