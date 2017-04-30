dbe7484eb23b911d9da4fc8144b479c1bd0ec546
cindyho97 <cindy.ho8197@gmail.com>
2017-05-01 00:36:57 +0200

    Add Shop Popup and test items

---
+++ Assets/Scripts/DisguiseItem.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+enum ItemState
+{
+    InShop, Bought, Selected, NotSelected
+};
+
+public class DisguiseItem : MonoBehaviour {
+
+    public Image disguiseImage;
+    public Text disguiseName;
+    public Text disguiseDescription;
+    public Text disguisePrice;
+    public GameObject button;
+    private Button buySelectButton;
+    private ItemState currentState;
+
+    public Sprite itemSprite;
+    public string itemName;
+    public string itemDescription;
+    public int price;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	public void ItemSetup()
+    {
+        disguiseImage.sprite = itemSprite;
+        disguiseName.text = itemName;
+        disguiseDescription.text = itemDescription;
+        disguisePrice.text = price.ToString();
+        buySelectButton = button.GetComponent<Button>();
+        CheckState();
+    }
+
+    public void CheckState()
+    {
+        if(currentState == ItemState.InShop)
+        {
+            buySelectButton.GetComponentInChildren<Text>().text = "Kopen";
+        }
+        else if (currentState == ItemState.Bought)
+        {
+            buySelectButton.GetComponentInChildren<Text>().text = "Gekocht";
+        }
+        else if (currentState == ItemState.NotSelected)
+        {
+            buySelectButton.GetComponentInChildren<Text>().text = "Aandoen";
+        }
+        else if (currentState == ItemState.Selected)
+        {
+            buySelectButton.GetComponentInChildren<Text>().text = "Uitdoen";
+        }
+    }
+}
---
+++ Assets/Scripts/ShopList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopList : MonoBehaviour {
+
+    public GameObject[] itemArray;
+    public Text totalCoinUI;
+    public Text totalCoinsShop;
+    private int itemCount;
+
+	// Use this for initialization
+	void Start () {
+        
+        SetupShop();
+	}
+
+    private void SetupShop()
+    {
+        totalCoinsShop.text = totalCoinUI.text;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).GetComponent<DisguiseItem>().ItemSetup();
+        }
+    }
+}
