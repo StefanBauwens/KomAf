02b0b71aa738528a6280a7bef6a7a897bda2a3c3
Stefan Bauwens <stefan.bauwens@hotmail.com>
2017-05-22 15:05:18 +0200

    Added winpopup

---
+++ Assets/Scripts/Popup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Popup : MonoBehaviour {
+	protected GameObject thisCanvas;
+
+	// Use this for initialization
+	void Start () {
+		thisCanvas = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Close()
+	{
+		thisCanvas.GetComponent<CanvasGroup> ().alpha = 0f;
+		thisCanvas.GetComponent<CanvasGroup> ().interactable = false;
+		thisCanvas.GetComponent<CanvasGroup>().blocksRaycasts = false;
+	}
+}
