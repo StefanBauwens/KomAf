f0986df80ba2b6dcaaf0dcd1f5dfb72e2620d3ee
Stefan Bauwens <stefan.bauwens@hotmail.com>
2017-05-25 21:27:29 +0200

    added changing lights in ruien

---
+++ Assets/Scripts/LightChanger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightChanger : MonoBehaviour {
+	public GameObject[] colorPrefabs;
+	public float waitTime;
+	protected int iterator;
+	protected GameObject colorfg;
+
+	// Use this for initialization
+	void Start () {
+		iterator = -1;
+		StartCoroutine (ChangeColor());
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+		
+	}
+
+	protected IEnumerator ChangeColor()
+	{
+		while (true) { 
+			if (iterator < colorPrefabs.Length - 1) {
+				if (colorfg != null) {
+					Destroy (colorfg, 0.0f);
+				}
+				iterator++;
+				colorfg = Instantiate (colorPrefabs [iterator], new Vector3 (-0.5f, -0.5f, -1), Quaternion.identity);
+			} else {
+				iterator = -1;
+			}
+			Debug.Log ("couroutine running");
+			yield return new WaitForSeconds(waitTime);
+
+		}
+		yield return null;
+	}
+}
