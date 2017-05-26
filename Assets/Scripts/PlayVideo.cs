134065a00f4208205261773e4fbf547b365b98df
Stefan Bauwens <stefan.bauwens@hotmail.com>
2017-05-26 17:55:57 +0200

    Pushed small changes

---
+++ Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVideo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Handheld.PlayFullScreenMovie ("test.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
